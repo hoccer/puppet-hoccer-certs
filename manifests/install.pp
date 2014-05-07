7677db98a64763c22e527f0d5ca1b59993a3ba64
Guido Lorenz <guido.lorenz@artcom.de>
2014-05-07 14:46:44 +0200

    Fix file source URL

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 class hoccer-certs::install {
   file { 'Hoccer CA':
     path => '/etc/ssl/certs/hoccer_ca.cer',
-    source => 'puppet:///module/hoccer-certs/hoccer_ca.cer'
+    source => 'puppet:///modules/hoccer-certs/hoccer_ca.cer'
   }
 }
